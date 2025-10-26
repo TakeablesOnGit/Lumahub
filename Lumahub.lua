1a66651254409200f70f804c62c98df0b5a7bb53
Takeables <TakeablesBusiness@gmail.com>
2025-10-26 16:37:43 -0500

    UI Init

---
+++ Lumahub.lua
@@ -0,0 +1,53 @@
+--// Settings \\--
+
+local HubName = "Lumahub"
+local HubAuthor = "Takeables"
+
+--// Frameworks \\--
+
+local UI_Framework =
+	loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+
+--// UI's <<--
+
+local Lumahub = UI_Framework:CreateWindow({
+	Title = HubName,
+	Author = "Made by " .. HubAuthor,
+	Folder = HubName,
+
+	KeySystem = {
+		Key = { "1234", "5678" },
+		Note = "Example Key System.",
+
+		Thumbnail = {
+			Image = "rbxassetid://",
+			Title = "Thumbnail",
+		},
+
+		URL = "YOUR LINK TO GET KEY (Discord, Linkvertise, Pastebin, etc.)",
+		SaveKey = true,
+	},
+})
+
+---------------------------[[ FARMING ]]---------------------------
+
+local LumaFarmingTab = Lumahub:Tab({
+	Title = "Farming",
+	Locked = false,
+})
+
+local LumaFarmingToggle = LumaFarmingTab:Toggle({
+	Title = "Farm Candy",
+	Desc = "collect candy currency automatically.",
+	Type = "Checkbox",
+	Value = false,
+
+	Callback = function(state)
+		print("Farming Candy Activated" .. tostring(state))
+	end,
+})
+
+--// Main \\--
+
+LumaFarmingTab:Select()
+LumaFarmingToggle:Set(true)
